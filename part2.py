1f4bd83e7ffa089595eef8a28e62d1fc539a4f05
apcurran21 <102324978+apcurran21@users.noreply.github.com>
2024-11-28 19:45:30 -0600

    3d simulation done, need to test

--- part2.py
+++ part2.py
@@ -12,15 +12,19 @@ NUM_CORES = 20
 
 
 def run_trial(params):
-    # currently `params` is just an integer
-    num_photons = params
+    # currently `params` is a tuple of a (integer, float)
+    num_photons, angle_rad = params
 
     n0 = 1.0
     n1 = 1.37
     start_time = time.time()
-    sim = utils.Simulation(num_photons, n0, n1)
+    # HARDCODED
+    Nz = 10000
+    Nx = int( Nz / 3 )
+    Ny = int( Nz / 3 )
+    sim = utils.Simulation(num_photons, n0, n1, Nx=Nx, Ny=Ny, Nz=Nz, incident_angle=angle_rad)
     sim.run_sim()
-    return (sim.A, sim.r_gb, sim.z_gb)
+    return sim.A
 
 
 if __name__ == "__main__":
@@ -35,38 +39,44 @@ if __name__ == "__main__":
     metadata={}
 
     # create params for the processes
-    num_photons_total = 1000000
-    # num_photons_total = 1000
-    num_photons_per = int( num_photons_total / NUM_CORES )  
-    params = [num_photons_per for _ in range(NUM_CORES)]
-    
-    # get accumulators for the trial results
-    A = np.zeros((200, 66))
-    r_gb = np.zeros(200)
-    z_gb = np.zeros(66)
-    start_time = time.time()
+    num_photons_total = 5000000
+    num_photons_per = int( num_photons_total / NUM_CORES )
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers=NUM_CORES) as executor:
-        trials_results = list(executor.map(run_trial, params))
-    
-    print("combining arrays")
-    for curr_A, curr_r_gb, curr_z_gb in trials_results:
-        A += curr_A
-        r_gb += curr_r_gb
-        z_gb += curr_z_gb
+    # define dimensions for the grid
+    Nz = 10000
+    Nx = int( Nz / 3 )
+    Ny = int( Nz / 3 )
 
-    # save to file
-    print("writing the arrays to disk...")
-    A_str = f"A_{num_photons_total}packets_{1.0}n0_{1.37}n1.npy".replace(".", "-")
-    rgb_str = f"rgb_{num_photons_total}packets_{1.0}n0_{1.37}n1.npy".replace(".", "-")
-    zgb_str = f"zgb_{num_photons_total}packets_{1.0}n0_{1.37}n1.npy".replace(".", "-")
-    np.save(output_path.joinpath(A_str), A)
-    np.save(output_path.joinpath(rgb_str), r_gb)
-    np.save(output_path.joinpath(zgb_str), z_gb)
+    # run simulations for each of the specified photon entry angles
+    angles = [20, 45, 70]
+    for angle in angles:
+        
+        print(f"Simulating {num_photons_total} with an incident angle of {angle} degrees in {NUM_CORES} processes ({num_photons_per} per process)...")
+        
+        # convert angle from degrees to radians
+        angle_rad = np.deg2rad(angle)
 
-    metadata['total_runtime'] = time.time() - start_time 
+        params = [(num_photons_per, angle_rad) for _ in range(NUM_CORES)]
+        
+        # get accumulators for the trial results
+        A = np.zeros((Nx + 2, Ny + 2, Nz + 1))         # choose (x, y, z) for simplicity
+        start_time = time.time()
 
-    with open(output_path.joinpath("experiment_metadata"), "w") as f:
-        json.dump(metadata, f)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=NUM_CORES) as executor:
+            trials_results = list(executor.map(run_trial, params))
+        
+        print("combining arrays")
+        for curr_A in trials_results:
+            A += curr_A
 
-    print("Finished the experiment!")
+        # save to file
+        print("writing the arrays to disk...")
+        A_str = f"A_{num_photons_total}packets_{1.0}n0_{1.37}n1.npy".replace(".", "-")
+        np.save(output_path.joinpath(A_str), A)
+
+        metadata['total_runtime'] = time.time() - start_time 
+
+        with open(output_path.joinpath("experiment_metadata"), "w") as f:
+            json.dump(metadata, f)
+
+        print("Finished the experiment!")
